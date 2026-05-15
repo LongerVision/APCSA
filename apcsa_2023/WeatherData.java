62ff49f4406eb9b33ac8eaf769891443e526b0d8
Pei Jia <jiapei@longervision.com>
2026-05-14 23:23:16 -0700

    Forgot to implement Part (b) of Q3

--- apcsa_2023/WeatherData.java
+++ apcsa_2023/WeatherData.java
@@ -73,7 +73,34 @@ public class WeatherData {
      */
     public int longestHeatWave(double threshold) {
         /* to be implemented in part (b) */
-        return 0;
+        int size = temperatures.size();
+        int maxLen = 0;
+        int len = 0;
+        boolean started = false;
+        double temp = 0.0;
+        for (int i = 0; i < size; i++) {
+            temp = temperatures.get(i);
+            if (temp > threshold) {
+                if (started)
+                    len++;
+                else {
+                    started = true;
+                    len = 1;
+                }
+            } else {
+                if (started) {
+                    started = false;
+                    if (len > maxLen)
+                        maxLen = len;
+                    len = 0;
+                }
+                // else {
+                // len = 0;
+                // }
+            }
+        }
+
+        return maxLen;
     }
 
     public void printValidData() {
@@ -86,8 +113,12 @@ public class WeatherData {
     // shown.
 
     public static void main(String[] args) {
-        double[] dArr = { 99.1, 142.0, 85.0, 85.1, 84.6, 94.3, 124.9, 98.0, 101.0, 102.5 };
+        // double[] dArr = { 99.1, 142.0, 85.0, 85.1, 84.6, 94.3, 124.9, 98.0, 101.0,
+        // 102.5 };
+        double[] dArr = { 100.5, 98.5, 102.0, 103.9, 87.5, 105.2, 90.3, 94.88, 109.1, 102.1, 107.4, 93.2 };
         WeatherData wd = new WeatherData(dArr);
-        wd.cleanData(85.0, 120.0);
+        // wd.cleanData(85.0, 120.0);
+
+        System.out.println(wd.longestHeatWave(95.2));
     }
 }
